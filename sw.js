cc699f6506dbf5f945dd7da4739aed6427b5cf33
mohankdesign-ux <mohank.design@gmail.com>
2026-03-17 14:06:57 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+﻿const CACHE_NAME = 'tapwalk-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+            return response;
+        }
+        return fetch(event.request).then(networkResponse => {
+            if (event.request.url.startsWith('http') && networkResponse.status === 200) {
+                const responseClone = networkResponse.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+            }
+            return networkResponse;
+        }).catch(() => {
+            // Offline fallback
+        });
+      })
+  );
+});
